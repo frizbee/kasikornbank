a8e636ce6d18f6e46a92a9d3954ac9b52e6c3996
Frizbee Vivaldi <frizbee@vivaldi.net>
2017-01-24 13:38:54 +0700

    fix indentation for rspec files

--- spec/kasikornbank_spec.rb
+++ spec/kasikornbank_spec.rb
@@ -1,80 +1,80 @@
 require "spec_helper"
 
 describe Kasikornbank do
-	let(:bank) { Kasikornbank::Render.new(
-		{payment: "credit card",
-			back_url: "https://example.com/back",
-			amount: 10.99,
-			ip_address: "127.0.0.1",
-			details2: "Booking villa",
-			invmerchant: 987,
-			shop_id: "00",
-			payterm2: "10"
-		}
-	)}
+  let(:bank) { Kasikornbank::Render.new(
+    {payment: "credit card",
+      back_url: "https://example.com/back",
+      amount: 10.99,
+      ip_address: "127.0.0.1",
+      details2: "Booking villa",
+      invmerchant: 987,
+      shop_id: "00",
+      payterm2: "10"
+    }
+  )}
 
   it "has a version number" do
     expect(Kasikornbank::VERSION).not_to be nil
   end
 
   it "has args to start" do
-  	expect(bank).to_not be(nil)
+    expect(bank).to_not be(nil)
   end
 
   it "has correct amount" do
-  	expect(bank.args[0][:amount]).to eql(10.99)
+    expect(bank.args[0][:amount]).to eql(10.99)
   end
 
   it "has amount in cents to be correct" do
-  	expect(bank.amount_in_cents).to eq("000000001099")
+    expect(bank.amount_in_cents).to eq("000000001099")
   end
 
   it "has correct ip address" do
-  	expect(bank.remote_ip).to eql("127.0.0.1")
+    expect(bank.remote_ip).to eql("127.0.0.1")
   end
 
   it "has some payment details" do
-  	expect(bank.order_details).to_not be nil
+    expect(bank.order_details).to_not be nil
   end
 
   it "not nil for invoice" do
-  	expect(bank.invoice_number).to_not be nil
+    expect(bank.invoice_number).to_not be nil
   end
 
   it "has correct invoice numebr" do
-  	expect(bank.invoice_number).to eq("000000000987")
+    expect(bank.invoice_number).to eq("000000000987")
   end
 
   it "should return MD5 checksum and shouldn't be nil" do
-  	configure = Configuration.new
-  	Kasikornbank.configure { configure }
-  	expect(bank.checksum).to_not be nil 
+    configure = Configuration.new
+    Kasikornbank.configure { configure }
+    expect(bank.checksum).to_not be nil 
   end
 
   it "checksum should return 32 character string" do
-  	configure = Configuration.new
-  	Kasikornbank.configure { configure }
-  	expect(bank.checksum.length).to eq(32)
+    configure = Configuration.new
+    Kasikornbank.configure { configure }
+    expect(bank.checksum.length).to eq(32)
   end
 
   it "should return something when call form method" do
-  	expect(bank.form).to_not be nil
+    expect(bank.form).to_not be nil
   end
 
   it "shoud return shop id if provided" do
-  	expect(bank.shop_id).to_not be nil
+    expect(bank.shop_id).to_not be nil
   end
 
   it "has return shop_id" do
-  	expect(bank.shop_id).to eq("00")
+    expect(bank.shop_id).to eq("00")
   end
 
   it "has payterm2" do
-  	expect(bank.payterm2).not_to be nil
+    expect(bank.payterm2).not_to be nil
   end
 
   it "has payterm2 to be exac number" do
-  	expect(bank.payterm2).to eq("10")
+    expect(bank.payterm2).to eq("10")
   end
 
 end
--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -2,89 +2,87 @@ require "spec_helper"
 
 describe Kasikornbank::Response do
 
-	context "Kasikornbank Response on click go back button" do
-		let(:response1) { 
-			Kasikornbank::Response.new(
-				{HOSTRESP: "00", RESERVED1: "XXXXXXXXXXXX", AUTHCODE: "000002", RETURNINV: "000000012111", RESERVED2: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", CARDNUMBER: "4072XXXXXXXX4249", AMOUNT: "000000004155", THBAMOUNT: "000000000100", CURISO: "036", FXRATE: "000000283155", FILLSPACE: "VISACARDXXXXXXXXXXXX"
-				}
-			)
-		}
+  context "Kasikornbank Response on click go back button" do
+    let(:response1) {
+      Kasikornbank::Response.new(
+        {HOSTRESP: "00", RESERVED1: "XXXXXXXXXXXX", AUTHCODE: "000002", RETURNINV: "000000012111", RESERVED2: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", CARDNUMBER: "4072XXXXXXXX4249", AMOUNT: "000000004155", THBAMOUNT: "000000000100", CURISO: "036", FXRATE: "000000283155", FILLSPACE: "VISACARDXXXXXXXXXXXX"}
+      )
+    }
 
-		it "response1 should not be nil" do
-			expect(response1).not_to be nil
-		end
-		it "has success response code" do
-			expect(response1.params[:HOSTRESP]).to eq("00")
-		end
-		it "has to be success" do
-			expect(response1.kbank_response[:response]).to eq("success")
-		end
-		it "has to be correct invoice number" do
-			expect(response1.kbank_response[:invoice]).to eq("12111")
-		end
-		it "has to be correct amount" do
-			expect(response1.kbank_response[:amount]).to eq(41.55)
-		end
-		it "has to has authcode" do
-			expect(response1.kbank_response[:auth_code]).to eq("000002")
-		end
-	end
+    it "response1 should not be nil" do
+      expect(response1).not_to be nil
+    end
+    it "has success response code" do
+      expect(response1.params[:HOSTRESP]).to eq("00")
+    end
+    it "has to be success" do
+      expect(response1.kbank_response[:response]).to eq("success")
+    end
+    it "has to be correct invoice number" do
+      expect(response1.kbank_response[:invoice]).to eq("12111")
+    end
+    it "has to be correct amount" do
+      expect(response1.kbank_response[:amount]).to eq(41.55)
+    end
+    it "has to has authcode" do
+      expect(response1.kbank_response[:auth_code]).to eq("000002")
+    end
+  end
 
-	context "Kasikornbank Response with SSL callback" do
-		let(:response2) { 
-			Kasikornbank::Response.new(
-				{PMGWRESP2: "01104921010005900017340000100764000000121362310120081602424973XXXXXXXX3855XXXXXXXXXXX00000008740000000002001XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXTest PaymentXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX10.198.205.168XXXXRKasikornbank Public Company LimitedXXXXXXXXXXXXXXXXXXXXXXXXXSociete GeneraleXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXTHAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXFRAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX000630303030303030303030303030303030303030300000000000000000000000000000000000000000"}
-			)
-		}
+  context "Kasikornbank Response with SSL callback" do
+    let(:response2) {
+      Kasikornbank::Response.new(
+        {PMGWRESP2: "01104921010005900017340000100764000000121362310120081602424973XXXXXXXX3855XXXXXXXXXXX00000008740000000002001XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXTest PaymentXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX10.198.205.168XXXXRKasikornbank Public Company LimitedXXXXXXXXXXXXXXXXXXXXXXXXXSociete GeneraleXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXTHAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXFRAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX000630303030303030303030303030303030303030300000000000000000000000000000000000000000"}
+      )
+    }
 
-		it "has response2 and shouldn't be nil" do
-			expect(response2).not_to be nil
-		end
-		it "has correct mertchan id" do
-			expect(response2.params[:PMGWRESP2][4...19]).to eq("492101000590001")
-		end
-		it "has to be success" do
-			expect(response2.kbank_response[:response]).to eq("success")
-		end
-		it "has to be correct invocie number" do
-			expect(response2.kbank_response[:invoice]).to eq("121362")
-		end
-		it "has to be correct amount" do
-			expect(response2.kbank_response[:amount]).to eq(874.0)
-		end
-		it "should has auth code nil" do
-			expect(response2.kbank_response[:auth_code]).to be nil
-		end
-	end
+    it "has response2 and shouldn't be nil" do
+      expect(response2).not_to be nil
+    end
+    it "has correct mertchan id" do
+      expect(response2.params[:PMGWRESP2][4...19]).to eq("492101000590001")
+    end
+    it "has to be success" do
+      expect(response2.kbank_response[:response]).to eq("success")
+    end
+    it "has to be correct invocie number" do
+      expect(response2.kbank_response[:invoice]).to eq("121362")
+    end
+    it "has to be correct amount" do
+      expect(response2.kbank_response[:amount]).to eq(874.0)
+    end
+    it "should has auth code nil" do
+      expect(response2.kbank_response[:auth_code]).to be nil
+    end
+  end
 
-	context "Kasikornbank Response without SSL standart callback" do
-		let(:response3) {
-			Kasikornbank::Response.new(
-				{PMGWRESP: "00XXXXXXXXXXXX000002XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX00000000011431012008143451000000008882XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXVISACARDXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX000000251400036000000283033"}
-			)
-		}
+  context "Kasikornbank Response without SSL standart callback" do
+    let(:response3) {
+      Kasikornbank::Response.new(
+        {PMGWRESP: "00XXXXXXXXXXXX000002XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX00000000011431012008143451000000008882XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXVISACARDXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX000000251400036000000283033"}
+      )
+    }
 
-		it "has response3 and shouldn't be nil" do
-			expect(response3).not_to be nil
-		end
-		it "has success standart response" do
-			expect(response3.params[:PMGWRESP][0...2]).to eq("00")
-		end
-		it "has AUD (036) currency in standart response" do
-			expect(response3.params[:PMGWRESP][206...209]).to eq("036")
-		end
-		it "has to be success" do
-			expect(response3.kbank_response[:response]).to eq("success")
-		end
-		it "has to be correct invoice number" do
-			expect(response3.kbank_response[:invoice]).to eq("114")
-		end
-		it "has to be correct amount" do
-			expect(response3.kbank_response[:amount]).to eq(88.82)
-		end
-		it "has auth code" do
-			expect(response3.kbank_response[:auth_code]).to eq('000002')
-		end
-	end
-
+    it "has response3 and shouldn't be nil" do
+      expect(response3).not_to be nil
+    end
+    it "has success standart response" do
+      expect(response3.params[:PMGWRESP][0...2]).to eq("00")
+    end
+    it "has AUD (036) currency in standart response" do
+      expect(response3.params[:PMGWRESP][206...209]).to eq("036")
+    end
+    it "has to be success" do
+      expect(response3.kbank_response[:response]).to eq("success")
+    end
+    it "has to be correct invoice number" do
+      expect(response3.kbank_response[:invoice]).to eq("114")
+    end
+    it "has to be correct amount" do
+      expect(response3.kbank_response[:amount]).to eq(88.82)
+    end
+    it "has auth code" do
+      expect(response3.kbank_response[:auth_code]).to eq('000002')
+    end
+  end
 end
